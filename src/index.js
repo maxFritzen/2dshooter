3fcc3d7ebefccd206a0277d8819adeb5d76168dd
maxFritzen <max.fritzen@hotmail.se>
2020-08-23 19:25:39 +0200

    feat: Player move by input, enemy follow player very simple

--- src/index.js
+++ src/index.js
@@ -1,30 +1,112 @@
 
-import { init, Sprite, GameLoop } from 'kontra';
+import kontra, { init, angleToTarget, Sprite, GameLoop } from 'kontra';
 import '../styles.css'
 
 let { canvas, context } = init();
+kontra.initKeys();
 
-let sprite = Sprite({
-  x: 100,        // starting x,y position of the sprite
-  y: 80,
-  color: 'red',  // fill color of the sprite rectangle
-  width: 20,     // width and height of the sprite rectangle
-  height: 40,
-  dx: 2          // move the sprite 2px to the right every frame
-});
+class Obj extends Sprite.class {
+  constructor (x, y, color, width, height, speed) {
+    super();
+    this.x = x
+    this.y = y
+    this.color = color
+    this.width = width
+    this.height = height
+    this.anchor = {x: 0.5, y: 0.5}
+    this.speed = speed;
+  }
+  stayInsideMap () {
+    if (this.x <= 0) this.x = 1;
+    if (this.x + this.width >= this.context.canvas.width) {
+      this.x = this.context.canvas.width - this.width
+    }
+    if (this.y <= 0) this.y = 1;
+    if (this.y + (this.height / 2) >= this.context.canvas.height) {
+      this.y = this.context.canvas.height - this.height
+    }
+  }
+  update () {
+    this.move()
+    this.stayInsideMap()
+  }
+} 
 
+
+class Enemy extends Obj {
+  constructor (x, y, color, width, height, speed, target) {
+    super(x, y, color, width, height, speed)
+    this.target = target
+  }
+
+  move() {
+    
+    this.dx = this.target.x - this.x;
+    this.dy = this.target.y - this.y;
+
+    this.rotation = Math.atan2(this.dy,this.dx);
+
+    if (this.x <= this.target.x) this.x += 1 * this.speed;
+    if (this.x >= this.target.x) this.x -= 1 * this.speed;
+    if (this.y <= this.target.y) this.y += 1 * this.speed;
+    if (this.y >= this.target.y) this.y -= 1 * this.speed;
+    
+  }
+}
+
+class Player extends Obj {
+  constructor (x, y, color, width, height, speed) {
+    super(x, y, color, width, height, speed)
+  }
+  move() {
+    if (kontra.keyPressed('left')) {
+      this.rotation += kontra.degToRad(-4);
+      const cos = Math.cos(this.rotation);
+      const sin = Math.sin(this.rotation);
+      this.dx = cos * 0.5;
+      this.dy = sin * 0.5;
+    } else if (kontra.keyPressed('right')) {
+      this.rotation += kontra.degToRad(4);
+      const cos = Math.cos(this.rotation);
+      const sin = Math.sin(this.rotation);
+      this.dx = cos * 0.5;
+      this.dy = sin * 0.5;
+    } 
+      
+    if (kontra.keyPressed('up')) {
+      this.advance(this.speed);
+    } else if (kontra.keyPressed('down')) {
+      this.advance(-this.speed * 0.7);
+    }
+  }
+}
+
+const newPlayer = new Player(
+  150,
+  250,
+  'blue',
+  5,
+  25,
+  2
+)
+
+const enemy = new Enemy(
+  50,
+  50,
+  'red',
+  10,
+  25,
+  0.3,
+  newPlayer
+)
 let loop = GameLoop({  // create the main game loop
   update: function() { // update the game state
-    sprite.update();
-
-    // wrap the sprites position when it reaches
-    // the edge of the screen
-    if (sprite.x > canvas.width) {
-      sprite.x = -sprite.width;
-    }
+    enemy.update();
+    newPlayer.update();
   },
   render: function() { // render the game state
-    sprite.render();
+    newPlayer.render();
+    enemy.render();
   }
 });
 
